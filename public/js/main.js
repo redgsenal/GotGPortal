2fad49d187f8c479e7b8dad7997170f2f0d7f6d2
ddb_tribal_reggie_senal <reggie.senal@sg.tribalworldwide.com>
2020-05-04 18:47:05 +0800

    update gotg

--- public/js/main.js
+++ public/js/main.js
@@ -14,4 +14,110 @@ $(function() {
     		console.log('drop this ', t);
     	}
     });
+
+    function buildpieces(pieces, color) {
+    	console.log(pieces);
+    	var board = $('.board');
+    	var colorside = color + 'piece';
+    	$.each(pieces, function(i, v){    		
+    		var loc = board.find('[data-location=' + v.location.x + '-' + v.location.y + ']');
+    		// console.log('location ', loc);
+    		if (loc.length){
+    			var p = loc.find('.piece');
+    			console.log('piece ', p);
+    			if (p.length) {
+    				p.addClass('draggable');
+    				p.addClass(colorside);
+    			}
+    		}
+    	});
+    }
+
+    function buildplayers(players){
+    	console.log(players);
+    	$.each(players, function(i, v){    		
+    		var c = $('.player#'+v.side);
+    		c.append('<span class="name">' + v.name + '</span>');
+    		buildpieces(v.pieces, v.side);
+    	});
+    }
+
+    function buildgame(game){
+    	console.log(game);
+    	buildplayers(game.board.players);
+    };
+
+    var game = {
+    	board: {
+    		players: [{
+    			name: 'John',
+	    		side: 'white',
+	    		pieces: 
+	    		[
+	    			{
+		    			location: {
+		    				x: 2,
+		    				y: 3
+		    			}
+	    			},
+	    			{
+	    				location: {
+	    					x: 3,
+	    					y: 2
+	    				}
+	    			},
+	    			{
+	    				location: {
+	    					x: 4,
+	    					y: 2
+	    				}
+	    			},
+	    			{
+	    				location: {
+	    					x: 5,
+	    					y: 2
+	    				}
+	    			},
+	    			{
+	    				location: {
+	    					x: 6,
+	    					y: 1
+	    				}
+	    			}
+	    		]
+    		},
+    		{
+    			name: 'Joe',
+	    		side: 'black',
+	    		pieces: 
+	    		[
+	    			{
+	    				location: {
+	    					x: 2,
+	    					y: 6
+	    				}
+	    			},
+	    			{
+		    			location: {
+		    				x: 6,
+		    				y: 6
+		    			}
+	    			},
+	    			{
+	    				location: {
+	    					x: 7,
+	    					y: 6
+	    				}
+	    			},
+	    			{
+		    			location: {
+		    				x: 8,
+		    				y: 6
+		    			}
+	    			}
+	    		]
+    		}]
+	    }
+	};
+	buildgame(game);
 });
