b1c6b39456cc561c6818d7d321e3b45e4c315e5f
ddb_tribal_reggie_senal <reggie.senal@sg.tribalworldwide.com>
2020-05-04 16:53:14 +0800

    update gotg

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,17 @@
 $(function() {
-	$( ".draggable" ).draggable({
-		addClasses: false
+	$('.draggable').draggable({
+		addClasses: false,
+		snap: 'td.cell',
+		drag: function(event, ui){
+			var t = $(this);
+			console.log('drag this ', t);
+		}
 	});
+
+	$('.droppable').droppable({
+    	drop: function(event, ui) {    		
+    		var t = $(this);    		
+    		console.log('drop this ', t);
+    	}
+    });
 });
